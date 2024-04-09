fd9fbf117963178f80114ff4ccde3165db14f292
Gustalex <alexandregustavo00@gmail.com>
2024-04-09 14:17:51 -0300

    add uma nova view que foca e uma receita e organiza views.py e url.py para receber a view

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,9 @@
+
 from django.urls import path
-from recipes.views import home
+from . import views
 
 urlpatterns = [
-    path('', home),# Home
-
+    path('', views.home),# Home
+    # ter cuidado para passar o tipo do id correto para evitar erros de segurança
+    path('recipes/<int:id>/', views.recipe),# Recipe
 ]
