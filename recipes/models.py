825f92213b1819c4cd3fe6f0eff5090ae778e2a3
Gustalex <alexandregustavo00@gmail.com>
2024-04-19 21:37:38 -0300

    comenta com os metodos de models e suas propriedades

--- recipes/models.py
+++ recipes/models.py
@@ -18,10 +18,26 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now = True)
     is_published = models.BooleanField(default = False)
     cover = models.ImageField(upload_to = 'recipes/covers/%Y/%m/%d/')
+    # category recebe a chave estrangeira da tabela Category e é setado como nulo para que possa ser criado um registro sem que seja necessário informar a categoria
     category = models.ForeignKey(
         Category, on_delete = models.SET_NULL, null = True
     )
+    # author recebe a chave estrangeira da tabela User e é setado como nulo para que possa ser criado um registro sem que seja necessário informar o autor
     author = models.ForeignKey(
         User, on_delete = models.SET_NULL, null = True
     )
+
+    # se caso tivessemos um models.CASCADE, ao deletar um usuário, todos os registros de receitas associados a ele seriam deletados
+
+    # se caso tivessemos um models.PROTECT, ao deletar um usuário, não seria possível deletar um usuário que tenha receitas associadas a ele
+
+    # se caso tivessemos um models.SET_NULL, ao deletar um usuário, os registros de receitas associados a ele teriam o campo author setado como nulo
+
+    # se caso tivessemos um models.SET_DEFAULT, ao deletar um usuário, os registros de receitas associados a ele teriam o campo author setado com um valor padrão
+
+    # se caso tivessemos um models.SET, ao deletar um usuário, os registros de receitas associados a ele teriam o campo author setado com um valor específico
+    
+    # se caso tivessemos um models.DO_NOTHING, ao deletar um usuário, os registros de receitas associados a ele não seriam afetados
+    
+
 # Create your models here.
